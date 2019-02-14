f61f348e75805814eff82c98e00b56160b5d2e19
sibalajp <sibalajp@gmail.com>
2019-02-14 15:43:02 -0800

    adding animations to submit

--- js/main.js
+++ js/main.js
@@ -84,7 +84,29 @@ $('#message_form').submit(function() {
             status('Error: ' + xhr.status)
           },
            success: function(response) {
-             console.log("Success")
+            $(function() {
+              $( "#submit" ).click(function() {
+                $( "#submit" ).addClass( "onclic", 250, validate);
+              });
+
+              function validate() {
+                setTimeout(function() {
+                  $( "#submit" ).removeClass( "onclic" );
+                  $( "#submit" ).addClass( "validate", 450, callback );
+                }, 2250 );
+              }
+                function callback() {
+                  setTimeout(function() {
+                    $( "#submit" ).removeClass( "validate" );
+                  }, 1250 );
+                }
+              });
+
+
+
+
+
+
          }
     });
     return false
